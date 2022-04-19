48174d8918ea81e544b09057553385de273f214b
LHH <8365179+lirenfu@user.noreply.gitee.com>
2022-04-19 18:30:39 +0800

    平衡二叉树

--- offer/src/Node/test617.java
+++ offer/src/Node/test617.java
@@ -2,6 +2,13 @@ package Node;
 
 public class test617 {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        return null;
+         if(root1==null)
+             return root2;
+         if(root2==null)
+             return root1;
+         TreeNode merged=new TreeNode(root1.val+root2.val);
+         merged.left=mergeTrees(root1.left,root2.right);
+         merged.right=mergeTrees(root1.right,root1.right);
+         return merged;
     }
 }
