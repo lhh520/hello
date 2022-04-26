ad44c379005327e029accf5298c1833017fe8528
lhh520 <lhh31415926@outlook.com>
2022-04-26 21:20:03 +0800

    2022 4 26

--- offer/src/testproject/最长回文串.java
+++ offer/src/testproject/最长回文串.java
@@ -2,16 +2,23 @@ package testproject;
 
 public class 最长回文串 {
     public static void main(String[] args) {
-        for(int i=1;i<=143;i++){
-            if(i<10){
-                System.out.println("000"+i+".png"+";"+"000"+i+"_GT.png");
+        for(int i=1;i<=204;i++){
+            if(i<52){
+                System.out.println("1_"+i+"_cut.jpg"+" "+"0");
             }
-            if(i>=10&&i<100){
-                System.out.println("00"+i+".png"+";"+"00"+i+"_GT.png");
+            if(i>=52&&i<96){
+                System.out.println("2_"+(i-51)+"_cut.jpg"+" "+"1");
             }
-            if(i>=100){
-                System.out.println("0"+i+".png"+";"+"0"+i+"_GT.png");
+            if(i>=96&&i<174){
+                System.out.println("3_"+(i-95)+"_cut.jpg"+" "+"2");
             }
+            if(i>=174&&i<198){
+                System.out.println("4_"+(i-173)+"_cut.jpg"+" "+"3");
+            }
+            if(i>198){
+                System.out.println("5_"+(i-198)+"_cut.jpg"+" "+"4");
+            }
+
         }
 
     }
