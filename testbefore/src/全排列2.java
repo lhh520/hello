ffe008b74b627467084ebb62575701665b6688a7
lhh520 <lhh31415926@outlook.com>
2023-04-14 19:56:37 +0800

    快手

--- testbefore/src/全排列2.java
+++ testbefore/src/全排列2.java
@@ -1,3 +1,30 @@
+import java.util.*;
 public class 全排列2 {
-
+    List<List<Integer>>ret=new ArrayList<>();
+    List<Integer>temp=new ArrayList<>();
+    boolean[]flag;
+    public List<List<Integer>> permute(int[] nums) {
+        if(nums.length==0){
+            return ret;
+        }
+        flag=new boolean[nums.length];
+        backtrace(nums);
+        return ret;
+    }
+    public void backtrace(int[] nums){
+        if(temp.size()==nums.length){
+            ret.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(flag[i]){//表示已经被使用过了
+                continue;
+            }
+            flag[i]=true;
+            temp.add(nums[i]);
+            backtrace(nums);
+            flag[i]=false;
+            temp.remove(temp.size()-1);
+        }
+    }
 }
